b3b1b7e436a87cffbed9e2b9af23e05236201971
Blavksmith <melvin11278@gmail.com>
2025-04-05 17:44:43 +0700

    setup next, auth

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+import { useEffect } from "react";
+
+const AboutPage = () => {
+  console.log("AboutPage rendered");
+  return <div>AboutPage</div>;
+};
+
+export default AboutPage;
