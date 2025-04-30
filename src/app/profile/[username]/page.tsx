ce65fd8d29411d14e4828046b70e2f92777194ab
Melvin <melvin11278@gmail.com>
2025-04-30 21:56:54 +0700

    add fetch profile logic

---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,14 @@
+import { getDbUserById } from "@/actions/user.actions";
+import React from "react";
+
+function ProfilePage({ params }: { params: { username: string } }) {
+  console.log("params", params);
+
+  
+
+  return <div>
+
+  </div>;
+}
+
+export default ProfilePage;
